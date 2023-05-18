b7a17893d4fa97d1edac36d087d05c102d3d6111
Josh Grafman <52416198+Josh-Grafman@users.noreply.github.com>
2023-05-18 10:12:47 -0400

    Add function comment docs to similarBoats

--- force-app/main/default/lwc/similarBoats/similarBoats.js
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -9,6 +9,14 @@ export default class SimilarBoats extends NavigationMixin(LightningElement) {
   error;
   boatId;
 
+  /*===========================================================================
+    Author:			Josh Grafman, Upsource Solutions
+    Created Date:	2023-05-18
+    Description:	Getter and setter for recordId
+  
+    Called by:		Lightning Record Page
+    =========================================================================*/
+
   @api
   get recordId() {
     return this.boatId;
@@ -21,8 +29,14 @@ export default class SimilarBoats extends NavigationMixin(LightningElement) {
   @api
   similarBy;
 
-  // Wire custom Apex call, using the import named getSimilarBoats
-  // Populates the relatedBoats list
+  /*===========================================================================
+    Author:			Josh Grafman, Upsource Solutions
+    Created Date:	2023-05-18
+    Description:	Set related boats when boatId and similarBy are set
+  
+    Called by:		Wire Service
+    =========================================================================*/
+
   @wire(getSimilarBoats, { boatId: '$boatId', similarBy: '$similarBy' })
   similarBoats({ data, error }) {
     if (data) {
@@ -32,14 +46,39 @@ export default class SimilarBoats extends NavigationMixin(LightningElement) {
     }
   }
 
+  /*===========================================================================
+    Author:			Josh Grafman, Upsource Solutions
+    Created Date:	2023-05-18
+    Description:	Dynamically set title for lightning-card
+  
+    Called by:		lightning-card
+    =========================================================================*/
+
   get getTitle() {
     return 'Similar boats by ' + this.similarBy;
   }
+
+  /*===========================================================================
+    Author:			Josh Grafman, Upsource Solutions
+    Created Date:	2023-05-18
+    Description:	Return true if there are no similar boats, otherwise return
+                  false.
+  
+    Called by:		Component conditional rendering
+    =========================================================================*/
+
   get noBoats() {
     return !(this.relatedBoats && this.relatedBoats.length > 0);
   }
 
-  // Navigate to record page
+  /*===========================================================================
+    Author:			Josh Grafman, Upsource Solutions
+    Created Date:	2023-05-18
+    Description:	Navigate to Boat__c page
+  
+    Called by:		boatselect event from boatTile component
+    =========================================================================*/
+
   openBoatDetailPage(event) {
     const boatId = event.detail.boatId;
     this[NavigationMixin.Navigate]({
